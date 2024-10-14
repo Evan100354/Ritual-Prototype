51244c07ae9b50d34ff49348dea71e69e74daded
Evan100354 <evanlong100354@gmail.com>
2024-10-14 02:27:41 +0100

    Playtest Setup P1

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -7,12 +7,17 @@ public class StartGame : MonoBehaviour
     [Header("Components")]
     [SerializeField] private GameObject prefab;
     [SerializeField] private Transform spawnPoint;
+    [SerializeField] private int instanceCount = 100;
 
     void Update()
     {
         if (Input.GetKeyDown("space"))
         {
-            Instantiate(prefab, new Vector3(spawnPoint.position.x, spawnPoint.position.y, spawnPoint.position.z), Quaternion.identity);
+            Debug.Log("space");
+            for (int i = 0; i < instanceCount; i++)
+            {
+                Instantiate(prefab, new Vector3(spawnPoint.position.x + (Random.Range(-1.5f, 2f)),spawnPoint.position.y + (Random.Range(-2.5f,2.5f)), spawnPoint.position.z + (Random.Range(-5f, 5f))), Quaternion.identity);
+            }
         }
     }
 }
